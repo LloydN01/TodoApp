de80f6ddaae92e990944300bf776541fc079ae06
LloydN01 <lloydna0527@gmail.com>
2021-12-06 00:53:30 +0800

    worked on it a bit more, 38:30

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,8 @@ const Todo = () => {
     return(
         <div className="todo">
             <li className="todo-item">Complete the Todo List</li>
-            
+            <button type="button" className="add">+</button>
+            <button type="button" className="remove">-</button>
         </div>
     );
 }
